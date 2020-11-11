0065dc27e64600b99b3e134a05fca2c0e0cf2950
Animesh Saha <animeshsaha1990@gmail.com>
2020-11-11 23:07:33 +0530

    first commit

---
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.testproject;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.PersistableBundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView collageImage;
+    Bitmap mergedImages;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        collageImage = (ImageView)findViewById(R.id.imageView);
+
+        Button combineImage = (Button)findViewById(R.id.combineimage);
+        Bitmap bigImage = BitmapFactory.decodeResource(getResources(), R.drawable.second_img);
+        Bitmap smallImage = BitmapFactory.decodeResource(getResources(), R.drawable.first_img);
+        mergedImages = createSingleImageFromMultipleImages(bigImage, smallImage);
+
+        collageImage.setImageBitmap(mergedImages);
+        combineImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Save successfully!",Toast.LENGTH_SHORT).show();
+                ContextWrapper cw = new ContextWrapper(getApplicationContext());
+                File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+                File file = new File(directory, "Mergefile" + ".jpg");
+                if (!file.exists()) {
+                    Log.d("path", file.toString());
+                    FileOutputStream fos = null;
+                    try {
+                        fos = new FileOutputStream(file);
+                        mergedImages.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                        fos.flush();
+                        fos.close();
+                    } catch (java.io.IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+
+
+
+        });
+
+
+
+    }
+
+
+
+    private Bitmap createSingleImageFromMultipleImages(Bitmap firstImage, Bitmap secondImage){
+
+        Bitmap result = Bitmap.createBitmap(firstImage.getWidth(), firstImage.getHeight(), firstImage.getConfig());
+        Canvas canvas = new Canvas(result);
+        canvas.drawBitmap(firstImage, 0f, 0f, null);
+        canvas.drawBitmap(secondImage, 1150, 850, null);
+
+
+        return result;
+    }
+
+    }
+
+
+
